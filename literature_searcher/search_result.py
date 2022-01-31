2e5e5fc03429f91e2d17cb6fbb1ae0a1a819ee26
tc1421 <tc1421@ic.ac.uk>
2022-01-31 22:10:33 +0000

    implemented pdf download functionality running on local flask pandoc.

--- literature_searcher/search_result.py
+++ literature_searcher/search_result.py
@@ -1,5 +1,8 @@
+#from os import sendfile
+from asyncio import subprocess
 from flask import Blueprint, request, render_template, flash, send_file
 from literature_searcher import query_processor
+import subprocess
 
 bp = Blueprint("search_result", __name__)
 
@@ -14,14 +17,37 @@ def response():
         search_results = query_processor.process(query)
         return render_template("search_result.html", search_results=search_results)
     # If file is markdown
-    else:
+    elif format == "markdown":
         # need to generate a markdown file, save to disk, send to client by putting in filename for this function
         search_results = query_processor.process(query)
-        file = open( "/tmp/markdown_query.md", "w")
+        file = open( ".\\tmp\markdown_query.md", "w")
         # need to save write to makedown_query.md
         file.write(str(search_results))
         file.close()
-        return send_file("/tmp/markdown_query.md", mimetype = "markdown", as_attachment=True)
+        return send_file("..\\tmp\markdown_query.md", mimetype = "markdown", as_attachment=True)
+    #if pdf download request
+    else:
+        search_results = query_processor.process(query)
+        file = open( ".\\tmp\\markdown_query.md", "w")
+        #pandoc does not like newline char, need to write line by line
+        for l in search_results:
+            file.write(l)
+        file.close()
+        #pandoc arguments. relative paths not working. need to adjust when adding to docker.
+        args = [
+            "pandoc",
+            "C:\\Users\\taole\\Imperial_local\\2_NLP4MS\\devops-python-group-project\\tmp\\markdown_query.md",
+            "-s",
+            "-o",
+            "C:\\Users\\taole\\Imperial_local\\2_NLP4MS\\devops-python-group-project\\tmp\\pdf_query.pdf"
+            ]
+        #execute pandoc as a subprocess.
+        result = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #print error log
+        stdout, stderr = result.communicate()
+        print("pandoc stderr: " + str(stderr))
+
+        return send_file("..\\tmp\pdf_query.pdf", mimetype = "application/pdf", as_attachment=True)
 
 @bp.route("/markdown_result_page.md")
 def generate_large_csv():
